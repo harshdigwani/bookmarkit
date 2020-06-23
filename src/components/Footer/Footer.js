8572878b62a1f5332fcff9a343202b63c89719e8
Harsh <harshdigwani@gmail.com>
2020-06-23 20:16:17 +0530

    emoji bug fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div className="footer">
-            <span>Made With ❤️ by<span onClick={() => window.open("https://harshdigwani.github.io")}>&nbsp; &lt;Harsh/&gt;</span></span>
+            Made With  <span role="img" aria-label="love">❤️</span>  by<span onClick={() => window.open("https://harshdigwani.github.io")}>&nbsp; &lt;Harsh/&gt;</span>
         </div>
     )
 }
